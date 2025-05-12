b928c0911c985b98979801125609ad30c7c8a488
juandrzej <programming.yippee729@passinbox.com>
2025-05-12 18:24:09 +0200

    extend ensure installed mason tool

--- init.lua
+++ init.lua
@@ -725,8 +725,13 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'lua_ls',
         'pyright',
         'bashls',
+        'black',
+        'isort',
+        'mypy',
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
